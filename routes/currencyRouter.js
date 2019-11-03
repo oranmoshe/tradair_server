9b041688728ebb0ae108e69c0f9ff76ea2a061c4
Oran <oran@shekelonline.com>
2019-11-03 23:46:49 +0200

    enable cors

--- routes/currencyRouter.js
+++ routes/currencyRouter.js
@@ -2,10 +2,21 @@
 
 const express = require('express');
 const router = express.Router();
+var cors = require('cors')
+var whitelist = ['http://localhost:4200']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
 
 const { getAllCurrencies } = require('../controllers/currenciesController');
 
 // Note: this route is actually /albums due to our index.js setup
-router.get('/', getAllCurrencies);
+router.get('/',cors(whitelist), getAllCurrencies);
 
 module.exports = router;
