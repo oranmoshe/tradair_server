524ae63865024451ed345983b60a35691ddca909
Oran <oran@shekelonline.com>
2019-11-05 02:03:52 +0200

    refactor routers....

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const _app_folder = 'public/dist/tradair-client';
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', routes);
+
 app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
 app.use('/view', express.static( _app_folder+'/index.html'))
+
+
+app.use('/', routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
